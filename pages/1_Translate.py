290cd296179825c253f4c0efca8ed1849ce6a5a9
Han Lee <lee46@mail.usf.edu>
2023-11-19 11:49:01 -0500

    remove key

--- pages/1_Translate.py
+++ pages/1_Translate.py
@@ -4,7 +4,7 @@ import openai
 from docx import Document
 import base64
 import io  # Don't forget to import the 'io' module
-openai.api_key = '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'
+openai.api_key = 'put your own key here'
 textToReturn = 'test'
 def translate_text(input_text, source_language, target_language):
     # Formulate a prompt for translation
